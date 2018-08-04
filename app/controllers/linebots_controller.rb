9d89116006c2f68bcdad4a5162dc13a14ad6f627
Yusuke Miyazaki <ysk1180@gmail.com>
2018-08-05 07:48:15 +0900

    アマゾン商品出力テスト34

--- app/controllers/linebots_controller.rb
+++ app/controllers/linebots_controller.rb
@@ -48,20 +48,20 @@ class LinebotsController < ApplicationController
             type: 'image',
             originalContentUrl: images[0],
             previewImageUrl: images[0]
-          }, {
-            type: 'text',
-            text: titles[1]
-          }, {
-            type: 'image',
-            originalContentUrl: images[1],
-            previewImageUrl: images[1]
-          }, {
-            type: 'text',
-            text: titles[2]
-          }, {
-            type: 'image',
-            originalContentUrl: images[2],
-            previewImageUrl: images[2]
+          # }, {
+          #   type: 'text',
+          #   text: titles[1]
+          # }, {
+          #   type: 'image',
+          #   originalContentUrl: images[1],
+          #   previewImageUrl: images[1]
+          # }, {
+          #   type: 'text',
+          #   text: titles[2]
+          # }, {
+          #   type: 'image',
+          #   originalContentUrl: images[2],
+          #   previewImageUrl: images[2]
           }]
           client.reply_message(event['replyToken'], message)
         end
