6f32c485b0a976c1b227a923cffb56d1cbf55102
Yusuke Miyazaki <ysk1180@gmail.com>
2018-08-04 15:13:29 +0900

    [add]コントローラ・ルーティングのベース作成

---
+++ app/controllers/linebots_controller.rb
@@ -0,0 +1,38 @@
+class LinebotsController < ApplicationController
+  require 'line/bot'
+
+  # callbackアクションのCSRFトークン認証を無効
+  protect_from_forgery :except => [:callback]
+
+  def callback
+    body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
+    events = client.parse_events_from(body)
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = [{
+            type: 'text',
+            text: 'テスト'
+          }]
+          client.reply_message(event['replyToken'], message)
+        end
+      end
+    end
+    head :ok
+  end
+
+  private
+
+  def client
+    @client ||= Line::Bot::Client.new do |config|
+      config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+      config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+    end
+  end
+end
